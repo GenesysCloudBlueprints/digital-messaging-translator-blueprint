05a8981e76c3887c80a40301e9f71ef83d48dad1
AgnesCorpuz <38449514+AgnesCorpuz@users.noreply.github.com>
2023-01-10 09:34:55 +0800

    Update flow module

--- docs/terraform/modules/flow/flow.tf
+++ docs/terraform/modules/flow/flow.tf
@@ -1,5 +1,6 @@
 resource "genesyscloud_flow" "flow" {
   filepath = "${path.module}/InboundMessageFlow.yaml"
+  file_content_hash = filesha256("${path.module}/InboundMessageFlow.yaml")
   substitutions = {
     flow_name           = "web-messaging-translation-flow"
     default_language    = "en-us"
